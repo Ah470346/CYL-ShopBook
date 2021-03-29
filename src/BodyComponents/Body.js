30d10577c8d4eef3215d305bd8c009875dc1a433
unknown <truongdtct1230@gmail.com>
2021-03-29 12:35:40 +0700

    feature/RightContentComponent favorite book Done

--- src/BodyComponents/Body.js
+++ src/BodyComponents/Body.js
@@ -3,6 +3,9 @@ import './Body.css';
 import SlideShow from './IntroComponents/Slide.js';
 import Policy from './IntroComponents/Policy.js';
 import Category from './LeftContentComponents/Category.js';
+import FavoriteBook from './RightContentComponents/FavoriteBook.js';
+import book1 from '../images/bookFavorite1.jpg';
+import book2 from '../images/bookFavorite2.jpg';
 
 function Body({menuBook}){
     return (
@@ -20,6 +23,14 @@ function Body({menuBook}){
                     <Category menuBook={menuBook}></Category>
                 </div>
                 <div className="col-lg-9 content-right  order-lg-last order-first">
+                    <div className='row favorite-books'>
+                        <div className='col-md-7 favorite-books-brand left'>
+                            <FavoriteBook image={book1} title={'True Crime to Blow Your Mind'}></FavoriteBook>
+                        </div>
+                        <div className='col-md-5 favorite-books-brand right'>
+                            <FavoriteBook image={book2} title={"Celebrate Indigenous Peoples' Day"}></FavoriteBook>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
